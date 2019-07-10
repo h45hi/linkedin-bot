f96ed671c9bdfe5614df7510500f3727e4b00143
Nisit <kumarnisit@gmail.com>
2019-07-10 08:45:37 +0530

    changing from facebook to linkedin

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-
-URL = 'https://www.facebook.com'
-class FacebookBot:
+import import ipdb
+URL = 'https://www.linkedin.com/'
+class LinkedinBot:
 
 	def __init__(self, username, password):
 		self.username = username
@@ -13,20 +13,25 @@ class FacebookBot:
 	def login(self):
 		bot = self.bot
 		bot.get(URL)
-		time.sleep(5)
-		email = bot.find_element_by_name('email')
-		password = bot.find_element_by_name('pass')
+		time.sleep(2)
+		sign_in = bot.find_element_by_class_name("nav__button-secondary").click()
+		time.sleep(2)
+		email = bot.find_element_by_id("username")
 		email.send_keys(self.username)
+		time.sleep(2)
+		password = bot.find_element_by_id("password")
 		password.send_keys(self.password)
-		sign_in = bot.find_element_by_id('loginbutton')
-		sign_in.send_keys(Keys.RETURN)
-		time.sleep(5) # _3_16 _6a-y _3l2t  _18vj
-
-	def like(self,search_text):
+		time.sleep(2)
+		sign_in = bot.find_element_by_class_name("btn__primary--large.from__button--floating").click()
+		
+	def for_position(self,position):
+		ipdb.set_trace()
+		position.strip(' ')
 		bot = self.bot
-		bot.get(URL+'/search/posts/?q='+search_text+'&epa=SERP_TAB')
+		bot.get(URL+'search/results/people/?keywords='+position+'&origin=SWITCH_SEARCH_VERTICAL')
 
 
-t = FacebookBot('rajivg774@gmail.com', 'rajiv9871')
+t = LinkedinBot('kumarnisit@gmail.com', 'HPiZ_CuwsD5$dqT')
 t.login()
-t.like('naruto uzumaki')
+time.sleep(5)
+t.for_position('Python Developer')
