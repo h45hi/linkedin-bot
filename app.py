f0d94cdc28f5cc51c83e531b594e884eeffca029
Nisit <kumarnisit@gmail.com>
2019-07-30 22:11:14 +0530

    save as PDF limit 6

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import import ipdb
+
 URL = 'https://www.linkedin.com/'
+
 class LinkedinBot:
 
 	def __init__(self, username, password):
@@ -24,14 +25,34 @@ class LinkedinBot:
 		time.sleep(2)
 		sign_in = bot.find_element_by_class_name("btn__primary--large.from__button--floating").click()
 		
-	def for_position(self,position):
-		ipdb.set_trace()
+	def search_for(self,position):
 		position.strip(' ')
 		bot = self.bot
-		bot.get(URL+'search/results/people/?keywords='+position+'&origin=SWITCH_SEARCH_VERTICAL')
+		search_url = bot.get(URL+'search/results/people/?keywords='+position+'&origin=SWITCH_SEARCH_VERTICAL')
 
+	def download_resume(self):
+		bot = self.bot
+		# bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+		profiles = bot.find_elements_by_class_name("search-result__result-link")
+		unique_links = []
+
+		for person in profiles:
+			link = person.get_attribute('href')
+			if link not in unique_links:
+					unique_links.append(link)
+
+		for link in unique_links:
+			bot.get(link)
+			time.sleep(5)
+			more = bot.find_element_by_class_name("pv-s-profile-actions__overflow-toggle.artdeco-button").click()
+			time.sleep(2)
+			save_pdf = bot.find_element_by_class_name("pv-s-profile-actions--save-to-pdf").click()
+			time.sleep(5)
+				
 
 t = LinkedinBot('kumarnisit@gmail.com', 'HPiZ_CuwsD5$dqT')
 t.login()
 time.sleep(5)
-t.for_position('Python Developer')
+t.search_for('Deepak')
+time.sleep(5)
+t.download_resume()
